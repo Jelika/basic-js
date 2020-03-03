1f54218c165155661205c939aed6192bfc464c28
Jelika <smirnova.lika.23092001@gmail.com>
2020-03-03 14:48:11 +0300

    add: dream team solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+ if(Array.isArray(members)){
+     if(members.length==0) {
+         throw 'array is empty'} else {
+        let result = members.reduce((acc,el,index) => 
+     {
+        return typeof el === "string" ? ( el = el.replace(/\s/g, ''), acc += el[0].toUpperCase()): acc}, []);
+        return ( result === [] ) ? false : result.split('').sort().join('');
+    }
+}
+else return false ;
+}
