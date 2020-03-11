4c312e64cc953100b93c25d41d86062a8fc9ce23
Jelika <smirnova.lika.23092001@gmail.com>
2020-03-12 01:05:28 +0300

    add:transform array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,26 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform( arr ) {
+    if (!Array.isArray(arr)) throw new Error('not an array');
+    if (arr.length === 0) return [];
+    let resultArray=[];
+    for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+        case '--discard-prev':
+            resultArray.pop();
+            break;
+        case '--discard-next':
+            i++;
+            break;
+        case '--double-next':
+            ((i+1) < arr.length)? resultArray.push(arr[i+1]):resultArray;
+            break;
+        case '--double-prev':
+            (i !== 0)? resultArray.push(arr[i - 1]): resultArray;
+            break;
+        default:
+            resultArray.push(arr[i]);
+            break;
+    }
+}
+return resultArray;
 };
+
