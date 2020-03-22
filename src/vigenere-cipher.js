3a0b357f9935b2b8bdf702ed58af9cdeb78761d5
Jelika <smirnova.lika.23092001@gmail.com>
2020-03-22 16:55:59 +0300

    feat: vigenere task solved

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,35 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(order = true) {
+        this.order = order;
+    }
+    encrypt(message, key) {
+        if (message == null || key == null)
+            throw new Error;
+        let result = [];
+        let keyIndex = 0;
+        for (let index = 0; index < message.length; index++) {
+            if (message[index].match(/[A-Za-z]/)) {
+                result[index] = String.fromCharCode((message[index].toUpperCase().charCodeAt() + key[keyIndex % key.length].toUpperCase().charCodeAt() - 65 * 2) % 26 + 65);
+                keyIndex++;
+            } else
+                result[index] = message[index];
+        }
+        return this.order ? result.join('') : result.reverse().join('');
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(message, key) {
+        if (message == null || key == null)
+            throw new Error;
+        let keyIndex = 0;
+        let result = [];
+        for (let index = 0; index < message.length; index++) {
+            if (message[index].match(/[A-Za-z]/)) {
+                result[index] = String.fromCharCode((26 + message[index].toUpperCase().charCodeAt() - key[keyIndex % key.length].toUpperCase().charCodeAt()) % 26 + 65);
+                keyIndex++;
+            } else
+                result[index] = message[index];
+        }
+        return this.order ? result.join('') : result.reverse().join('');
     }
 }
 
